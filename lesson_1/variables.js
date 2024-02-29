265e2636635cead009bf7ca42f7948d014a02847
Bedri Bayryamov <81079469+bedri-s-b@users.noreply.github.com>
2024-02-29 20:27:26 +0200

    Lesson 1

---
+++ lesson_1/variables.js
@@ -0,0 +1,20 @@
+
+        // -DECLARATION, INITIALIZATION, ASSINGMENT
+
+//DECLARATION on variable is registers a veriable in the corresponding scope
+let veriable;
+
+//INITIALIZATION is allocates memory for the variable
+let veriableTwo = 'some initialization';
+
+//ASSINGMENT a scecified value to the variable
+veriable = 7347; //new value
+
+//REASSINGMENT
+veriableTwo = "changing the previous value"; //new value
+
+        // types of data
+
+let firstName = "Bedri"; //strings
+let age = 32; // numbers
+let student = false; // booleans
